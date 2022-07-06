adc65b4532d70c0f61e6c4a0aafd29e1466408a5
Alex Rønne Petersen <alex@alexrp.com>
2022-07-06 14:16:03 +0200

    Add a sample testing framework-dependent deployment.
    Closes #12.

---
+++ src/samples/dependent/InjectedProgram.cs
@@ -0,0 +1,53 @@
+sealed class InjectedProgram : IInjectedProgram
+{
+    static readonly TimeSpan _timeout = TimeSpan.FromMinutes(1);
+
+    public static async Task<int> RunAsync(InjectedProgramContext context, ReadOnlyMemory<string> args)
+    {
+        if (context.InjectorProcessId != null)
+            return 42;
+
+        Console.WriteLine("Starting conhost.exe...");
+
+        using var proc = new Process()
+        {
+            StartInfo = new()
+            {
+                FileName = "conhost.exe",
+            },
+        };
+
+        _ = proc.Start();
+
+        try
+        {
+            if (!proc.WaitForInputIdle(_timeout))
+                throw new TimeoutException();
+
+            Console.WriteLine("Started as {0}. Injecting...", proc.Id);
+
+            using var target = TargetProcess.Open(proc.Id);
+            using var injector = new AssemblyInjector(
+                target,
+                new AssemblyInjectorOptions(typeof(InjectedProgram).Assembly.Location)
+                    .WithInjectionTimeout(_timeout)
+                    .WithCompletionTimeout(_timeout));
+
+            await injector.InjectAssemblyAsync();
+
+            Console.WriteLine("Injected.");
+
+            var code = await injector.WaitForCompletionAsync();
+
+            Console.WriteLine("Returned code {0}.", code);
+
+            return code == 42 ? 0 : 1;
+        }
+        finally
+        {
+            proc.Kill(true);
+
+            await proc.WaitForExitAsync();
+        }
+    }
+}
