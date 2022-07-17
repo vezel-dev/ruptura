b6a320dbd04ccfce0b4fa5d9f7f14444d112172d
Alex Rønne Petersen <alex@alexrp.com>
2022-07-17 13:13:47 +0200

    Change the trace sample to test CallTrace from a FunctionHook.

--- src/samples/trace/Program.cs
+++ src/samples/trace/Program.cs
@@ -1,11 +1,45 @@
-static class Program
+static unsafe class Program
 {
+    static FunctionHook? _hook;
+
+    static CallTrace? _trace;
+
     public static int Main()
     {
-        var trace = CallTrace.Capture();
+        var lib = NativeLibrary.Load("kernel32.dll");
 
-        Console.WriteLine(trace);
+        try
+        {
+            using var manager = new PageCodeManager();
 
-        return trace.Frames.Count > 20 && trace.Frames[0].ManagedMethod?.Name == nameof(Main) ? 0 : 1;
+            var func = (delegate* unmanaged[Stdcall]<nint, char*, int>)NativeLibrary.GetExport(
+                lib, "SetThreadDescription");
+
+            using (_hook = FunctionHook.Create(
+                manager, func, (delegate* unmanaged[Stdcall]<nint, char*, int>)&SetThreadDescriptionHook))
+            {
+                _hook.IsActive = true;
+
+                _ = func(-1, null);
+            }
+        }
+        finally
+        {
+            NativeLibrary.Free(lib);
+        }
+
+        return _trace?.Frames is { Count: > 20 } && _trace.Frames[0].ManagedMethod?.Name == "SetThreadDescriptionHook"
+            ? 0
+            : 1;
+    }
+
+    [UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvStdcall) })]
+    static int SetThreadDescriptionHook(nint hThread, char* lpThreadDescription)
+    {
+        _trace = CallTrace.Capture();
+
+        Console.WriteLine(_trace);
+
+        return 0;
     }
 }
